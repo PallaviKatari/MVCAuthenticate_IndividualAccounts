9ffbd95d9fbf38630a4a654ab6926c19b5170cfe
PallaviKatari <pallavikatari@gmail.com>
2023-09-06 15:51:08 +0530

    Add project files.

---
+++ Controllers/AuthorizeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCAuthenticate_IndividualAccounts.Controllers
+{
+    public class AuthorizeController : Controller
+    {
+        // GET: Authorize
+        [Authorize(Users ="john@gmail.com")]
+        public ActionResult Index()
+        {
+            return Content("Authorize Users Only");
+        }
+        [AllowAnonymous]
+        public ActionResult Index1()
+        {
+            return Content("Public Users");
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCAuthenticate_IndividualAccounts.Startup))]
+namespace MVCAuthenticate_IndividualAccounts
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
